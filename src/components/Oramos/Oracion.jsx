c57c749ae9dbdd252aba5768ebc94d99a386f933
GinoGallardo <ginosgt@gmail.com>
2025-02-24 18:34:57 -0300

    funcionalidad del boton de la seccion oramos

--- src/components/Oramos/Oracion.jsx
+++ src/components/Oramos/Oracion.jsx
@@ -12,11 +12,15 @@ export default function Oracion() {
       <h4 className='text-center text-red-600 font-bold font-serif sm:text-2xl md:text-3xl lg:text-4xl'>Oramos por Vos</h4>
       <span className='text-center text-white text-2xl font-serif font-extrabold md:text-wrap md:text-4xl lg:text-6xl'>Mateo 18:20</span>
       <h2 className='text-center text-white text-2xl font-serif font-extrabold md:text-wrap md:text-4xl lg:text-6xl'>Necesitas ayuda o querés que oremos por vos?</h2>
-      <button className="flex items-center justify-center md:gap-4 bg-red-600 py-2 px-4 rounded-md cursor-pointer hover:bg-red-400 text-2xl font-semibold">
+      <a 
+        href="https://api.whatsapp.com/send?phone=01121697373"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center justify-center md:gap-4 bg-red-600 py-2 px-4 rounded-md cursor-pointer hover:bg-red-400 text-2xl font-semibold"
+      >
         <IoArrowForwardCircleOutline size={30}/>
         Charla con nosotros
-      </button>
-
+      </a>
       </div>
     </section>
   )
